60ba1d20e4ec466a909138963494e5c322331488
afra2604 <109967726+afra2604@users.noreply.github.com>
2023-10-12 02:38:38 +0700

    Second commit

--- app/src/main/java/com/example/uts_pppb/InputDataActivity.kt
+++ app/src/main/java/com/example/uts_pppb/InputDataActivity.kt
@@ -1,11 +1,92 @@
 package com.example.uts_pppb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import com.example.uts_pppb.databinding.ActivityInputDataBinding
 
 class InputDataActivity : AppCompatActivity() {
+
+    private lateinit var fillMakanan: EditText
+    private lateinit var fillJumlah:EditText
+    private lateinit var spinJenis: Spinner
+    private lateinit var spinWaktu:Spinner
+    private lateinit var fillWO:EditText
+    private lateinit var fillDurasi:EditText
+    private lateinit var spinWaktuWO: Spinner
+    private lateinit var fillKaloriJumlah: EditText
+    private lateinit var btnSimpan1: Button
+
+
+    private lateinit var binding: ActivityInputDataBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input_data)
+        binding = ActivityInputDataBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fillMakanan = findViewById(R.id.fill_makanan)
+        fillJumlah = findViewById(R.id.fill_jumlah)
+        spinJenis = findViewById(R.id.spin_jenis)
+        spinWaktu = findViewById(R.id.spin_waktu)
+        fillWO = findViewById(R.id.fill_wo)
+        fillDurasi = findViewById(R.id.fill_durasi)
+        spinWaktuWO = findViewById(R.id.spin_waktu_wo)
+        fillKaloriJumlah = findViewById(R.id.fill_kalori_jumlah)
+        btnSimpan1 = findViewById(R.id.btn_simpan1)
+
+
+        val jenisKaloriSpinner = findViewById<Spinner>(R.id.spin_jenis)
+        val jenisKaloriList =
+            listOf("Karbohidrat", "Lemak", "Mineral", "Protein", "Vitamin", "Zat besi")
+        val jenisKaloriAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, jenisKaloriList)
+        jenisKaloriAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        jenisKaloriSpinner.adapter = jenisKaloriAdapter
+
+        val waktuMakanSpinner = findViewById<Spinner>(R.id.spin_waktu)
+        val waktuMakanList = listOf("Pagi", "Siang", "Malam")
+        val waktuMakanAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, waktuMakanList)
+        waktuMakanAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        waktuMakanSpinner.adapter = waktuMakanAdapter
+
+        val waktuWOSpinner = findViewById<Spinner>(R.id.spin_waktu_wo)
+        val waktuWOList = listOf("Pagi", "Siang", "Malam")
+        val waktuWOAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, waktuWOList)
+        waktuMakanAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        waktuWOSpinner.adapter = waktuWOAdapter
+
+        btnSimpan1.setOnClickListener {
+            val intent = Intent (this, MainActivity::class.java);
+            val namaMakanan = fillMakanan.text.toString()
+            val jumlahKalori = fillJumlah.text.toString()
+            val jenisKalori = jenisKaloriSpinner.selectedItem.toString()
+            val waktuMakan = waktuMakanSpinner.selectedItem.toString()
+            val namaWorkout = fillWO.text.toString()
+            val durasiWorkout = fillDurasi.text.toString()
+            val jumlahKaloriWorkout = fillKaloriJumlah.text.toString()
+            val waktuWorkout = waktuWOSpinner.selectedItem.toString()
+
+            // Menambahkan data ke Intent
+            intent.putExtra("nama_makanan", namaMakanan)
+            intent.putExtra("jumlah_kalori", jumlahKalori)
+            intent.putExtra("jenis_kalori", jenisKalori)
+            intent.putExtra("waktu_makan", waktuMakan)
+            intent.putExtra("nama_workout", namaWorkout)
+            intent.putExtra("durasi_workout", durasiWorkout)
+            intent.putExtra("jumlah_kalori_workout", jumlahKaloriWorkout)
+            intent.putExtra("waktu_workout", waktuWorkout)
+
+
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+
+
+
     }
-}
+    }
--- app/src/main/java/com/example/uts_pppb/MainActivity.kt
+++ app/src/main/java/com/example/uts_pppb/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.uts_pppb
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.uts_pppb.databinding.ActivityMainBinding
 
 import java.text.SimpleDateFormat
@@ -21,11 +24,41 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cardTujuan: TextView
     private lateinit var cardTanggalTercapai: TextView
     private lateinit var cardKalori: TextView
+    private lateinit var sisaKalori: TextView
+    private lateinit var txtKonsumsiKalori: TextView
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result ->
+            if (result.resultCode== Activity.RESULT_OK){
+                val data = result.data
+                val nama = data?.getStringExtra("nama")
+                val beratBadanSekarang = data?.getStringExtra("beratBadanSekarang")
+                val spinnerBbNow = data?.getStringExtra("spinnerBbNow")
+                val beratBadanTarget = data?.getStringExtra("beratBadanTarget")
+                val spinnerBbTarget = data?.getStringExtra("spinnerBbTarget")
+                val tujuanDiet = data?.getStringExtra("tujuanDiet")
+                val tanggalTarget = data?.getStringExtra("tanggalTarget")
+                val kaloriHarian = data?.getStringExtra("kaloriHarian")
+
+
+                binding.txtHalo.text = "Halo, $nama !"
+                binding.bbNow.text = "Berat badan saat ini : $beratBadanSekarang $spinnerBbNow"
+                binding.bbTarget.text = "Target berat badan : $beratBadanTarget $spinnerBbTarget"
+                binding.cardTujuan.text = "Tujuan diet : $tujuanDiet"
+                binding.cardTanggalTercapai.text = "Tanggal tercapai : $tanggalTarget"
+                binding.cardKalori.text = "Kalori harian : $kaloriHarian"
+
+            }
+
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
         timeNow = findViewById(R.id.time_now)
         dateNow = findViewById(R.id.date_now)
@@ -35,6 +68,8 @@ class MainActivity : AppCompatActivity() {
         cardTujuan = findViewById(R.id.card_tujuan)
         cardTanggalTercapai = findViewById(R.id.card_tanggal_tercapai)
         cardKalori = findViewById(R.id.card_kalori)
+        sisaKalori = findViewById(R.id.txt_sisa_kalori)
+        txtKonsumsiKalori = findViewById(R.id.txt_konsumsi_kalori)
 
         val currentTime = Date()
 
@@ -52,6 +87,10 @@ class MainActivity : AppCompatActivity() {
         val tanggalTarget = intent.getStringExtra("tanggalTarget")
         val spinnerBbNow = intent.getStringExtra("spinnerBbNow")
         val spinnerBbTarget = intent.getStringExtra("spinnerBbTarget")
+        val kaloriMasuk = intent.getStringExtra("fillJumlah")
+        val kaloriKeluar = intent.getBooleanArrayExtra("fillKaloriJumlah")
+//        val sisaKalori = kaloriHarian-kaloriMasuk
+        val btnInputData = findViewById<Button>(R.id.btn_input_data)
 ""
         // Menampilkan  yang diformat dalam TextView
 
@@ -67,7 +106,10 @@ class MainActivity : AppCompatActivity() {
             binding.cardKalori.text = "Kalori harian : $kaloriHarian"
 
             btnInputData.setOnClickListener{
-                val intent = Intent (this@MainActivity, InputDataActivity::class.java)
+
+                val intent = Intent(this@MainActivity, InputDataActivity::class.java)
+                    .apply { putExtra("nama") }
+                launcher.launch(intent)
 
             }
 
